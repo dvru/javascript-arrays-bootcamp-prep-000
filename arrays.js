cb421e53ce5f43fb64531588f3c25c24e24c26f0
Damini Varu <damvaru4@gmail.com>
2019-09-09 20:00:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,8 +3,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ]
 function addElementToBeginningOfArray(myArray1, myElement1) {
   const myNewArray = [myElement1, ...myArray1]
   return myNewArray
-
 }
+
 function destructivelyAddElementToBeginningOfArray(myArray2, myElement2){
   myArray2.unshift(myElement2)
   return myArray2
@@ -17,15 +17,23 @@ function destructivelyAddElementToEndOfArray(myArray4, myElement4){
   myArray4.push(myElement4)
   return myArray4
 }
+
+
 function accessElementInArray(myArray5, index5){
   return myArray5[index5]
 
 }
+
+
+
+
+
 function destructivelyRemoveElementFromBeginningOfArray(myArray6){
   myArray6.shift(myArray6)
   return myArray6
 
 }
+
 function removeElementFromBeginningOfArray(myArray7){
   myArray8 = myArray7.slice(1)
   return myArray8
